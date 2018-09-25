bfe3c5b120339565ad40648821273243a140cd6c
prashant kumar <prashantbicky@gmail.com>
2018-09-25 14:39:18 +0530

    Added searchpage feature code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,15 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	
+	public void Loginpage() {
+		System.out.println("loginpage");
+	}
+
+	public void loginSet() {
+		
+		System.out.println("login set");
+	}
+	
+	
 }
